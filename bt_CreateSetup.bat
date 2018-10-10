e80312fc235dab34251c112d591ae1d164c6139e
bee-eater <marcel_krausert@freenet.de>
2018-10-10 16:17:02 +0200

    Removed version from setup batch script

--- bt_CreateSetup.bat
+++ bt_CreateSetup.bat
@@ -3,7 +3,7 @@ echo.SystemdumpViewer Setup Builder v1.0
 echo.---------------------------------------------
 echo.
 echo.Building setup, please be patient...
-"..\zz_inno\ISCC.exe" "/DVERSION_NAME=%version%" .\50_inno\BR.SystemdumpViewer.iss >> NUL
+"..\zz_inno\ISCC.exe" .\50_inno\BR.SystemdumpViewer.iss >> NUL
 echo.
 echo.Done!
 echo.
