8ce8a0bfa1e3e2dbaa8df2335c455c0edfb462cd
bee-eater <marcel_krausert@freenet.de>
2021-01-14 13:44:03 +0100

    Updated splash screen, get version on main screen automatically from version string

--- 00_src/mainwindow.cpp
+++ 00_src/mainwindow.cpp
@@ -594,7 +594,7 @@ void MainWindow::on_actionAbout_triggered()
     about.setWindowTitle(tr("About ..."));
     about.setTextFormat(Qt::RichText);
     about.setText(QString("<br/><br/>")+tr("This application is not an official B&R product! For this reason the use is at your own risk and there is NO claim on support or maintenance.")+QString("<br/><br/>Version: %1 <br/><br/>App: %2").arg(this->Version,qApp->applicationFilePath()));
-    about.setInformativeText(tr("Developed with Qt 5.6")+QString("<br/><br/>")+tr("This program uses parts of the following Projects: ") + QString("<br/>- <a href='http://qwt.sf.net'>Qwt Project</a> (CPU-Usage graph)<br/>- <a href='http://rapidxml.sourceforge.net'>RapidXml</a><br/>- <a href='https://github.com/brofield/simpleini'>SimpleIni</a><br/><br/>Copyright %1 <a href='mailto:marcel_krausert@freenet.de'>Marcel Krausert</a> 2021").arg(QString::fromUtf8("\u00A9"))+QString("<br/><br/>")+tr("This work is licensed under the")+QString("<br/>&emsp;&emsp;&emsp;<a href='https://www.gnu.org/licenses/gpl-3.0.de.html'>GNU GENERAL PUBLIC LICENSE, Version 3</a><br/><br/>")+tr("Visit us on")+QString("<a href='https://github.com/bee-eater/SystemDumpViewer'>GitHub</a>!"));
+    about.setInformativeText(tr("Developed with Qt 5.6")+QString("<br/><br/>")+tr("This program uses parts of the following Projects: ") + QString("<br/>- <a href='http://qwt.sf.net'>Qwt Project</a> (CPU-Usage graph)<br/>- <a href='http://rapidxml.sourceforge.net'>RapidXml</a><br/>- <a href='https://github.com/brofield/simpleini'>SimpleIni</a><br/><br/>Copyright %1 <a href='mailto:marcel_krausert@freenet.de'>Marcel Krausert</a> 2021").arg(QString::fromUtf8("\u00A9"))+QString("<br/><br/>")+tr("This work is licensed under the")+QString("<br/>&emsp;&emsp;&emsp;<a href='https://www.gnu.org/licenses/gpl-3.0.de.html'>GNU GENERAL PUBLIC LICENSE, Version 3</a><br/><br/>")+tr("Visit us on")+QString("<a href='https://github.com/bee-eater/SystemDumpViewer'> GitHub</a>!"));
     about.setStandardButtons(QMessageBox::Ok);
     about.setIconPixmap(QPixmap("://images/about.png").scaledToWidth(128));
     about.setDefaultButton(QMessageBox::Ok);
@@ -1764,7 +1764,8 @@ void MainWindow::startScreenInit(){
     ypos = ypos + 50;
     this->start_sdvLogo->move(xpos,ypos);
     this->start_sdvLogo->setAlignment(Qt::AlignCenter);
-    this->start_sdvLogo->setText("SdV v2.2");
+    QStringList Versions = this->Version.split(".");
+    this->start_sdvLogo->setText("SdV v" + Versions[0] + "." + Versions[1]);
 
     this->start_brLogo = new QLabel(this);
     this->start_brLogo->resize(118,67);
